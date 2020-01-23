7e97b5f559e776593fa5ddd497ab732c6c6ffd12
zkx <z18161825600@163.com>
2020-01-23 09:52:34 +0800

    second commit

---
+++ src/main/java/com/example/books_management/mapper/BorrowBooksMapper.java
@@ -0,0 +1,8 @@
+package com.example.books_management.mapper;
+
+import com.example.books_management.pojo.BorrowBooks;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface BorrowBooksMapper extends CommonMapper<BorrowBooks> {
+}
